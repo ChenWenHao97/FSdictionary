c52f3def19b5be1abd38893f72cbc2dc68bf1fd7
WenHaoChen97 <www.572043666mo@qq.com>
2020-07-21 21:00:24 +0800

    add pbrpc

--- src/client.cpp
+++ src/client.cpp
@@ -1,17 +1,18 @@
 #include"utils/common.hpp"
 #include"utils/zk_base.hpp"
+#include"utils/echo_service.pb.h"
 int main(int argc,char** argv)
 {
-    if (argc < 2)
-    {
-        std::cout << "input server address and port!" << std::endl;
-    }
+    // if (argc < 2)
+    // {
+    //     std::cout << "input server address and port!" << std::endl;
+    // }
     zk_base *base_handle = new zk_base();
-    const char *host = argv[1];
-    int timeout = 300;
+    const char *zk_host = "127.0.0.1:2181";
+    int timeout = 3000;
     const char *context = "client_query";
     zoo_set_debug_level(ZOO_LOG_LEVEL_WARN);
-    zhandle_t *zk_handle = base_handle->zk_init(host,
+    zhandle_t *zk_handle = base_handle->zk_init(zk_host,
                                                 reinterpret_cast<watcher_fn>(zk_base::watcher_global),
                                                 timeout, 0, (void *)context, 0);
     const char * father_node_name = "/election";
@@ -38,8 +39,54 @@ int main(int argc,char** argv)
     char *get_node_info = new char[256];
     int get_node_len;
     base_handle->zk_get(zk_handle, path.c_str(), 1, get_node_info, &get_node_len, &stat);
-    std::cout<<"get_node_info:"<<get_node_info<<std::endl;//获取到提供服务的ip地址以及端口
+    std::cout<<"################################################"<<std::endl;
+    std::cout<<"###############get_node_info####################"<<std::endl;
+    std::cout<<get_node_info<<std::endl;//获取到提供服务的ip地址以及端口
+    const char* server_host = get_node_info;
 
 
+
+    //rpc
+    // SOFA_PBRPC_SET_LOG_LEVEL(NOTICE);
+
+    // 定义RpcClient对象，管理RPC的所有资源
+    // 通常来说，一个client程序只需要一个RpcClient实例
+    // 可以通过RpcClientOptions指定一些配置参数，譬如线程数、流控等
+    sofa::pbrpc::RpcClientOptions client_options;
+    client_options.work_thread_num = 2;
+    sofa::pbrpc::RpcClient rpc_client(client_options);
+
+    // 定义RpcChannel对象，代表一个消息通道，需传入Server端服务地址
+    sofa::pbrpc::RpcChannel rpc_channel(&rpc_client, server_host);
+ 
+    // 定义EchoServer服务的桩对象EchoServer_Stub，使用上面定义的消息通道传输数据
+    fs::EchoServer_Stub stub(&rpc_channel);
+ 
+    // 定义和填充调用方法的请求消息
+    fs::EchoRequest request;
+   std::cout<<"################################################"<<std::endl;
+    std::cout<<"###############send client message####################"<<std::endl;
+    request.set_message("send from  client message");
+ 
+    // 定义方法的回应消息，会在调用返回后被填充
+    fs::EchoResponse response;
+ 
+    // 定义RpcController对象，用于控制本次调用
+    // 可以设置超时时间、压缩方式等；默认超时时间为10秒，默认压缩方式为无压缩
+    sofa::pbrpc::RpcController controller;
+    controller.SetTimeout(3000);
+ 
+    // 发起调用，最后一个参数为NULL表示为同步调用
+    stub.Echo(&controller, &request, &response, NULL);
+
+    // 调用完成后，检查是否失败
+    if (controller.Failed()) {
+        // 调用失败后的错误处理，譬如可以进行重试
+        SLOG(ERROR, "request failed: %s", controller.ErrorText().c_str());
+    }
+   std::cout<<"################################################"<<std::endl;
+    std::cout<<"###############get client message####################"<<std::endl;
+    std::cout<<"echo message:"<<response.message()<<std::endl;
+    return EXIT_SUCCESS;
 
 }
--- src/server_can_selection.cpp
+++ src/server_can_selection.cpp
@@ -1,14 +1,34 @@
 #include "utils/common.hpp"
 #include "utils/zk_base.hpp"
+#include"utils/echo_service.pb.h"
 void get_node_name(char *buf, char *node_name);
 void is_leader(zhandle_t *zk_handle, const char *father_node_name, const char *child_node, const char *set_registry_info);
 void election_child_watcher(zhandle_t *zk_handle, int type, int state, const char *path, void *watcherCtx);
-struct watcher_node_info
-{
+struct watcher_node_info{
     zhandle_t *handle;
     std::string father_node_name;
     std::string child_node_name;
     std::string set_registry_info;
+};
+class EchoServerImpl:public fs::EchoServer{
+    public:
+        EchoServerImpl(){}
+        virtual ~EchoServerImpl(){}
+    private:
+        virtual void Echo(google::protobuf::RpcController* controller,
+                            const fs::EchoRequest* request,
+                            fs::EchoResponse* response,
+                            google::protobuf::Closure* done)
+        {
+            sofa::pbrpc::RpcController* cntl = 
+                static_cast<sofa::pbrpc::RpcController*>(controller);
+                std::cout<<"################################################"<<std::endl;
+                SLOG(NOTICE,"Echo():request message from %s:%s",
+                    cntl->RemoteAddress().c_str(),request->message().c_str());
+                response->set_message("server response message:"+request->message());
+                done->Run();//通知rpc系统服务完成，出发发送response
+        }
+
 };
 int main(int argc, char **argv)
 {
@@ -17,11 +37,12 @@ int main(int argc, char **argv)
         std::cout << "input server address and port!" << std::endl;
     }
     zk_base *base_handle = new zk_base();
-    const char *host = argv[1];
+    const char *zk_host = "127.0.0.1:2181";
+    const char *server_host=argv[1];
     int timeout = 300;
     const char *context = "zk_selection";
     zoo_set_debug_level(ZOO_LOG_LEVEL_WARN);
-    zhandle_t *zk_handle = base_handle->zk_init(host,
+    zhandle_t *zk_handle = base_handle->zk_init(zk_host,
                                                 reinterpret_cast<watcher_fn>(zk_base::watcher_global),
                                                 timeout, 0, (void *)context, 0);
     char *buf = new char[256];
@@ -36,10 +57,9 @@ int main(int argc, char **argv)
                            ZOO_SEQUENCE | ZOO_EPHEMERAL,
                            buf,
                            256);
-
     char *child_node_name = new char[256];
     get_node_name(buf, child_node_name);//获取创建的子节点名字
-    const char *set_registry_info = host;
+    const char *set_registry_info = server_host;
 
     is_leader(zk_handle, father_node_name.c_str(), child_node_name, set_registry_info);//判断是否为leader节点
     struct String_vector child_infos;
@@ -47,15 +67,38 @@ int main(int argc, char **argv)
     node_info.handle = zk_handle;
     node_info.father_node_name = father_node_name;
     node_info.child_node_name = child_node_name;
-    node_info.set_registry_info = host;
+    node_info.set_registry_info = server_host;
     struct Stat stat; //制定节点信息
     //得到指定node信息和状态
     base_handle->zk_wgetchild2(zk_handle, father_node_name.c_str(), election_child_watcher, &node_info, &child_infos, &stat);
-    sleep(10000);
+
+    //rpc部分
+    SOFA_PBRPC_SET_LOG_LEVEL(NOTICE);
+
+    sofa::pbrpc::RpcServerOptions options;
+    options.work_thread_num = 2;
+    sofa::pbrpc::RpcServer rpc_server(options);
+
+    if(!rpc_server.Start(server_host))
+    {
+        SLOG(ERROR,"start server failed");
+        return EXIT_FAILURE;
+    }
+    fs::EchoServer* echo_service = new EchoServerImpl();
+    if(!rpc_server.RegisterService(echo_service))
+    {
+        SLOG(ERROR,"register server failed");
+        return EXIT_FAILURE;
+    }
+    rpc_server.Run();
+    rpc_server.Stop();
+
     base_handle->zk_close(zk_handle);
     
-    return 0;
+    return EXIT_SUCCESS;
 }
+
+
 void get_node_name(char *buf, char *node_name)
 {
 
